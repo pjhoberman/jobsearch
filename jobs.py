b5fabdf4d888baa2e8c1140f8e4506da472edf0f
PJ Hoberman <pj@twoparts.com>
2020-05-13 12:41:03 -0600

    Refactored code to be Class-based and utilize multiprocessing

--- jobs.py
+++ jobs.py
@@ -3,63 +3,92 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-jobs_json = None
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) ' \
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) ' \
              'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-headers = {'User-Agent': user_agent, "Content-Type": "text/html"}
+HEADERS = {'User-Agent': USER_AGENT, "Content-Type": "text/html"}
 
 
-def get_site_text(url):
-    response = requests.get(url, headers=headers)
-    if response.status_code != 200:
-        print("{}: {}".format(response.status_code, url))
-        return response.status_code
+class Site:
+    """
+    Object should be instantiated with a url like this:
+    s = Site("http://www.randomword.com")
+    On init, object checks the site text against the previous text stored in a json file.
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    return list(soup.stripped_strings)
+    """
+    def __init__(self, url):
+        self.url = url
+        self._new_text = None
+        self._old_text = None
+        self.diff = None
 
+        if self.old_text and self.old_text != self.new_text:
+            self.create_readable_diff()
 
-def create_readable_diff(a, b):
-    diffs = [{"-": p[0], "+": p[1]} for p in zip(a, b) if p[0] != p[1]]
-    if len(a) > len(b):
-        diffs += [{"-": x, "+": ""} for x in a[len(b) - len(a):]]
-    elif len(b) > len(a):
-        diffs += [{"-": x, "+": ""} for x in b[len(a) - len(b):]]
-    return diffs
+    @property
+    def old_text(self):
+        """
+        Property declaration to grab old text from json file based on self.url
+        :return: list - Beautiful Soup list of strings stored in json file
+        """
+        if not self._old_text:
+            with open("jobs.json", "r") as file:
+                self._old_text = json.loads(file.read()).get(self.url, None)
+        return self._old_text
 
+    @property
+    def new_text(self):
+        """
+        Property declaration to grab new text from self.url
+        :return: list - Beautiful Soup list of strings from site
+        """
+        if not self._new_text:
+            response = requests.get(self.url, headers=HEADERS)
+            if response.status_code != 200:
+                print("{}: {}".format(response.status_code, self.url))
+                self._new_text = response.status_code
+            else:
+                soup = BeautifulSoup(response.text, 'html.parser')
+                self._new_text = list(soup.stripped_strings)
+        return self._new_text
 
-def check_site(url):
-    global jobs_json
-
-    text = get_site_text(url)
-    diff = {"url": url, "diff": None}
-    if jobs_json.get(url) and jobs_json.get(url) != text:
-        diff['diff'] = create_readable_diff(jobs_json.get(url), text)
-    jobs_json[url] = text
-
-    return diff
+    def create_readable_diff(self):
+        """
+        Creates a low-level diff comparison of self.old_text and self.new_text
+        Note: This isn't a super clean diff. One day it will look like a git diff.
+            For now, it's more of an attempt to show what lines changed.
+        :return: list of dicts showing the + and - comparison
+        """
+        a = self.old_text
+        b = self.new_text
+        diffs = [{"-": p[0], "+": p[1]} for p in zip(a, b) if p[0] != p[1]]
+        if len(a) > len(b):
+            diffs += [{"-": x, "+": ""} for x in a[len(b) - len(a):]]
+        elif len(b) > len(a):
+            diffs += [{"-": x, "+": ""} for x in b[len(a) - len(b):]]
+        self.diff = diffs
+        return diffs
 
 
 def check_sites():
-    global jobs_json
-    with open("urls.txt", "r") as file, open("jobs.json", "r") as j:
+    """
+    Function to grab the urls from urls.txt,
+    create Site objects with a multiprocessing pool,
+    then store some info back into jobs.json and print the results
+    :return: None. Prints results
+    """
+    with open("urls.txt", "r") as file:
         urls = [line.strip() for line in file]
-        jobs_json = json.loads(j.read())
 
-    # Removed for now - need to figure out global vars with multiprocessing, or some other solution
-    # pool = mp.Pool(mp.cpu_count())
-    # changed = pool.map(check_site, urls)
-    # pool.close()
-    changed = []
-    for url in urls:
-        changed.append(check_site(url))
+    pool = mp.Pool(mp.cpu_count())
+    sites = pool.map(Site, urls)
+    pool.close()
 
     with open("jobs.json", "w") as file:
-        file.write(json.dumps(jobs_json))
+        file.write(json.dumps({site.url: site.new_text for site in sites}))
 
-    for change in changed:
-        print("### ", change['url'], " ###")
-        print(change['diff'])
+    for site in sites:
+        print("### ", site.url, " ###")
+        print(site.diff)
 
 
 if __name__ == '__main__':
